a502dc00ce64308d947404924ea6d7fbbd047a19
Alex Belke <alex.belke.de@gmail.com>
2019-06-27 20:41:35 +0200

    create meals table in initDB.sql

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,3 +1,4 @@
+DROP TABLE if exists meals;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
@@ -23,3 +24,18 @@ CREATE TABLE user_roles
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+
+create table meals
+(
+    id INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    date_time timestamp not null,
+    description varchar not null,
+    calories integer not null,
+    user_id integer not null
+        constraint meals_users_id_fk
+            references users
+            on delete cascade
+);
+
+
